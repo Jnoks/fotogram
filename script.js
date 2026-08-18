67aa68d7e6dbe6db3eab3a5e0ff247ed5e934bc8
Jnoks <joel@jnoks.dev>
2026-08-18 20:41:54 +0200

    finished desktop version of fotogram

--- script.js
+++ script.js
@@ -49,34 +49,55 @@ const IMAGES = [
   },
 ];
 
+let currentImageIndex = 0;
+
 function generatePhotoAlbum() {
   let html = "";
   for (let i = 0; i < IMAGES.length; i++) {
-    html += getPhotoTemplate(IMAGES[i]);
+    html += getPhotoTemplate(IMAGES[i], i);
   }
   document.getElementById("photo-album").innerHTML = html;
 }
 
-function getPhotoTemplate(image) {
+function getPhotoTemplate(image, index) {
   return /*html*/ `
-    <img class="thumbnail" src="${image.src}" alt="${image.alt}">
+    <img class="thumbnail" onclick="openDialog(${index})" src="${image.src}" alt="${image.alt}">
   `;
 }
 
-function openDialog() {
+function openDialog(index) {
+  currentImageIndex = index;
+  updateDialog();
+  document.getElementById("image-dialog").showModal();
+}
 
+function updateDialog() {
+  const img = IMAGES[currentImageIndex];
+  document.getElementById("dialog-image").src = img.src;
+  document.getElementById("dialog-image").alt = img.alt;
+  document.getElementById("dialog-title").innerHTML = img.alt;
+  document.getElementById("image-counter").innerHTML =
+    currentImageIndex + 1 + "/" + IMAGES.length;
 }
 
 function closeDialog() {
-  
+  document.getElementById("image-dialog").close();
 }
 
 function showNextImage() {
-  
+  currentImageIndex++;
+  if (currentImageIndex >= IMAGES.length) {
+    currentImageIndex = 0;
+  }
+  updateDialog();
 }
 
 function showPreviousImage() {
-  
+  currentImageIndex--;
+  if (currentImageIndex < 0) {
+    currentImageIndex = IMAGES.length - 1;
+  }
+  updateDialog();
 }
 
 generatePhotoAlbum();
